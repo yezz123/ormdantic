d0687dcc24925b7aec8459c15edc94bc3dacdd3d
Yasser Tahiri <yasserth19@gmail.com>
2022-08-22 22:54:03 +0100

    🔖 Release version 1.1.0

--- ormdantic/__init__.py
+++ ormdantic/__init__.py
@@ -1,6 +1,6 @@
 """asynchronous ORM that uses pydantic models to represent database tables ✨"""
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 from ormdantic.orm import Ormdantic
 
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,4 +2,4 @@ import ormdantic
 
 
 def test_version() -> None:
-    assert ormdantic.__version__ == "1.0.0"
+    assert ormdantic.__version__ == "1.1.0"
