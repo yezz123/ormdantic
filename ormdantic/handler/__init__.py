b5c2426509011e4e35fa0f43eeb3ee3cccd69625
Yasser Tahiri <yasserth19@gmail.com>
2022-08-31 19:14:01 +0100

    :bug: fix Import issue

--- ormdantic/handler/__init__.py
+++ ormdantic/handler/__init__.py
@@ -5,16 +5,11 @@ from ormdantic.handler.errors import (
     TypeConversionError,
     UndefinedBackReferenceError,
 )
-from ormdantic.handler.helper import (
-    Get_M2M_TableName,
-    Model_Instance,
-    TableName_From_Model,
-)
+from ormdantic.handler.helper import Model_Instance, TableName_From_Model
 from ormdantic.handler.snake import snake as snake_case
 
 __all__ = [
     "TableName_From_Model",
-    "Get_M2M_TableName",
     "ConfigurationError",
     "UndefinedBackReferenceError",
     "MismatchingBackReferenceError",
